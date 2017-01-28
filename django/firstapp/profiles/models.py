454217e82af32651a835cc85e25f0902ebd19daf
Saif Rehman <s4saif.121@gmail.com>
2017-01-28 21:14:06 +0400

    init

--- django/firstapp/profiles/models.py
+++ django/firstapp/profiles/models.py
@@ -2,4 +2,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=120)
+    description = models.TextField(default='Desciption default text')
+
+    class Meta():
+        db_table = "profile"
+
+    def __unicode__(self):
+        return self.name
