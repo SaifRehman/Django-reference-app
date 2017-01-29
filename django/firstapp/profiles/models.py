c7896b2dbbe4f30e5e1762e81e7e41016fedc095
Saif Rehman <s4saif.121@gmail.com>
2017-01-29 14:21:45 +0400

    init

--- django/firstapp/profiles/models.py
+++ django/firstapp/profiles/models.py
@@ -1,15 +1,16 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
 
-# Create your models here.
 class Profile(models.Model):
     name = models.CharField(max_length=120)
-    description = models.TextField(default='Desciption default text')
+    location = models.CharField(default='location', max_length=120)
+    description = models.TextField(default='Description default text')
+    job = models.CharField(max_length=120, null=True, blank=True)
 
-    class Meta():
+    class Meta:
         db_table = "profile"
 
+    @property
     def __unicode__(self):
         return self.name
--- django/firstapp/profiles/views.py
+++ django/firstapp/profiles/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def Home(request):
+    context = locals()
+    template = "home.html"
+    return render(request, context, template)
